9da34b1809b65c8cffc61204f689d0d0f19e7a67
Dylan Bickerstaff <dylan@belowaverage.org>
2020-04-26 16:01:58 -0400

    Add project files.

---
+++ ImmediateAccess/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImmediateAccess
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                if (args.Contains("/debug"))
+                {
+                    //Main main = new Main();
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Main()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
