6d8493be3fe5c54ee05f920d8e52e7538e9179ee
Dylan Bickerstaff <dylan@belowaverage.org>
2020-04-26 21:15:07 -0400

    Doing Checks

--- ImmediateAccess/ImmediateAccess.cs
+++ ImmediateAccess/ImmediateAccess.cs
@@ -11,30 +11,37 @@ namespace ImmediateAccess
 {
     class ImmediateAccess
     {
-        public static void Start(string[] Paremeters)
+        private static bool IsNetworkAvailable = false;
+        public static async void Start(string[] Paremeters)
         {
-            Logger.Info("Initializing...");
+            Logger.Info("Testing probe availability...");
+            await TestNetwork.IsProbeAvailable();
+            Logger.Info("Registering event listeners...");
             NetworkChange.NetworkAvailabilityChanged += NetworkChange_NetworkAvailabilityChanged;
             NetworkChange.NetworkAddressChanged += NetworkChange_NetworkAddressChanged;
+            Logger.Info("Service is ready.", ConsoleColor.Green);
         }
-
-        private static void NetworkChange_NetworkAddressChanged(object sender, EventArgs e)
+        private static async void NetworkChange_NetworkAddressChanged(object sender, EventArgs e)
         {
             Logger.Info("Network address has changed.");
+            if (IsNetworkAvailable)
+            {
+                await TestNetwork.IsProbeAvailable();
+            }
         }
-
-        private static void NetworkChange_NetworkAvailabilityChanged(object sender, NetworkAvailabilityEventArgs e)
+        private static async void NetworkChange_NetworkAvailabilityChanged(object sender, NetworkAvailabilityEventArgs e)
         {
+            IsNetworkAvailable = e.IsAvailable;
             if (e.IsAvailable)
             {
                 Logger.Info("Network is now available.", ConsoleColor.Green);
+                await TestNetwork.IsProbeAvailable();
             }
             else
             {
                 Logger.Warning("Network is no longer available.");
             }
         }
-
         public static void Stop()
         {
 
---
+++ ImmediateAccess/TestNetwork.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace ImmediateAccess
+{
+    class TestNetwork
+    {
+        private static bool AlreadyTestingProbe = false;
+        private static Ping Ping = new Ping();
+        public static async Task<bool> IsProbeAvailable() {
+            bool result = false;
+            if (AlreadyTestingProbe) return false;
+            AlreadyTestingProbe = true;
+            int pingTimeout = 1000;
+            string probeAddress = "ad.belowaverage.org";
+            try
+            {
+                Logger.Info("Message sent to probe...");
+                PingReply reply = await Ping.SendPingAsync(probeAddress, pingTimeout);
+                
+                if (reply.Status == IPStatus.Success) result = true;
+            }
+            catch (Exception)
+            {
+                Logger.Warning("Probe failed to respond in a timely manner...");
+                result = false;
+            }            
+            if (result)
+            {
+                Logger.Info("\"" + probeAddress + "\" is available.", ConsoleColor.Green);
+            }
+            else 
+            {
+                Logger.Warning("\"" + probeAddress + "\" is unavailable.");
+            }
+            AlreadyTestingProbe = false;
+            return result;
+        }
+    }
+}
